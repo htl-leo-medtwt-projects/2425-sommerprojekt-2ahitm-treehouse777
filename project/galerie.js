3d0f148ccf1f0d5fc62b11d4f2e360712a8bab3f
Valerie Krolokh <v.krolokh@students.htl-leonding.ac.at>
2025-04-30 13:24:24 +0200

    Css

--- project/galerie.js
+++ project/galerie.js
@@ -86,19 +86,23 @@ function openHerbModal(herb) {
   
  modalBody.innerHTML = `
     <img src="${herb.img}" alt="${herb.name}">
+    <div id="info">
+        <ul>
+        <li><strong>Familie:</strong> ${herb.familie}</li>
+        <li><strong>Inhaltsstoffe:</strong> ${herb.inhaltsstoffe?.join(", ") || "–"}</li>
+        <li><strong>Weitere Namen:</strong> ${herb.weitereNamen?.join(", ") || "–"}</li>
+        <li><strong>Aussaatzeit:</strong> ${herb.aussaatzeit || "–"}</li>
+        <li><strong>Blütezeit:</strong> ${herb.blütezeit || "–"}</li>
+        <li><strong>Ernte:</strong> ${herb.ernte || "–"}</li>
+        <li><strong>Standort:</strong> ${herb.standort || "–"}</li>
+        <li><strong>Verwendung:</strong> ${herb.verwendung?.join(", ") || "–"}</li>
+        </ul>
+    </div>
     <h2>${herb.name}</h2>
     <h4><em>${herb.latein}</em></h4>
     <p>${herb.beschreibung}</p>
-    <ul>
-       <li><strong>Familie:</strong> ${herb.familie}</li>
-       <li><strong>Inhaltsstoffe:</strong> ${herb.inhaltsstoffe?.join(", ") || "–"}</li>
-       <li><strong>Weitere Namen:</strong> ${herb.weitereNamen?.join(", ") || "–"}</li>
-       <li><strong>Aussaatzeit:</strong> ${herb.aussaatzeit || "–"}</li>
-       <li><strong>Blütezeit:</strong> ${herb.blütezeit || "–"}</li>
-       <li><strong>Ernte:</strong> ${herb.ernte || "–"}</li>
-       <li><strong>Standort:</strong> ${herb.standort || "–"}</li>
-       <li><strong>Verwendung:</strong> ${herb.verwendung?.join(", ") || "–"}</li>
-    </ul>
+    
+    
     `;
     modal.style.display = "flex";
 }
