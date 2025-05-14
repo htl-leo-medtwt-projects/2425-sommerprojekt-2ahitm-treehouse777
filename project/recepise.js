562c88365d14e41b09318a6cdf5132690b724956
Valerie Krolokh <v.krolokh@students.htl-leonding.ac.at>
2025-05-14 13:39:11 +0200

    Rezepte Array

---
+++ project/recepise.js
@@ -0,0 +1,182 @@
+recepice = [
+    [
+        comment = "Salbe",
+        nameOfIt = "Thymiansalbe",
+        ingridiens = [
+            "50ml Thymian Ölauszug",
+            "8g Bienenwachs"
+        ],
+        forWhat = "Wirkt und pflegt bei fetter und unreiner Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Thymian Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Die Thymian Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Melissensalbe",
+        ingridiens = [
+            "50ml Melissen Ölauszug",
+            "8g Bienenwachs"
+        ],
+        forWhat = "Für fette und empfindliche Haut gut geeignet.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Melissen Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Die Melissen Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Honig Ringelblumensalbe",
+        ingridiens = [
+            "50ml Ringelblumen Ölauszug",
+            "8g Bienenwachs",
+            "1/2 Teelöffel Honig"
+        ],
+        forWhat = "Wirkt lindernd bei reizbarer und trockener Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Ringelblumen Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Auf 40 Grad Celsius abkühlen und den Honig dazu rühren. Die Honig Ringelblumen Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Holunderblütensalbe",
+        ingridiens = [
+            "50ml Holunderblüten Ölauszug",
+            "8g Bienenwachs"
+        ],
+        forWhat = "Wirkt lindernd bei reizbarer und trockener Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Holunderblüten Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Die Holunderblüten Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Rosenblütensalbe",
+        ingridiens = [
+            "50ml Rosenblüten Ölauszug",
+            "8g Bienenwachs"
+        ],
+        forWhat = "Sehr wertvolle Pflege für trockene und empfindliche Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Rosenblüten Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Die Rosenblüten Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Ringelblütensalbe",
+        ingridiens = [
+            "50ml Ringelblüten Ölauszug",
+            "8g Bienenwachs",
+            "eventuell 5Tropfen Propolis Tinktur"
+        ],
+        forWhat = "Gut geeignet bei unreiner und fettiger Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Ringelblumen Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Auf 40 Grad Celsius abkühlen und die Propolis Tinktur dazu rühren.  Die Ringelblumen Salbe etwas ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Lavendelblütensalbe",
+        ingridiens = [
+            "50ml Lavendelblüten Ölauszug",
+            "8g Bienenwachs"
+        ],
+        forWhat = "Beruhigt leicht reizbare Haut.",
+        zubereitung = "Bienenwachs im Wasserbad schmelzen. Lavendelblüten Ölauszug hinzufügen und alles auf 60 Grad Celsius erwärmen. Aus dem Wasserbad nehmen und Glatt rühren. Die Lavendel Salbe eine Stunde ruhen lassen, nochmals schonend durchrühren und in dunkle Glastiegel umfüllen."
+    ],
+    [
+        nameOfIt = "Salbe bei Pilzinfektionen",
+        ingridiens = [
+            "1Eßl. Wermut",
+            "1Eßl. Beifuß",
+            "1Eßl. Melisse",
+            "200ml Sojaöl",
+            "20ml Schwarzkümmelöl",
+            "10ml Propolis Tinktur",
+            "20g Bienenwachs",
+            "10Tropen ätherisches Teebaumöl"
+        ],
+        forWhat = "Bei Pilzinfektionen und Herpes mehrmals täglich auftragen.",
+        zubereitung = "Das Sojaöl im Wasserbad erwärmen. Bienenwachs dazugeben und schmelzen lassen. Danach die Kräuter dazugeben und ca. 2 Stunden darin ziehen lassen. Nochmals erwärmen und dann durch ein Tuch filtern. Schwarzkümmelöl langsam einrühren. Ständig rühren bis die Salbe ca. 40 Grad Celsius hat. Danach das ätherische Teebaumöl und die Propolis Tinktur einrühren und in dunkle Glastiegel abfüllen."
+    ],
+    [
+        nameOfIt = "Salbe bei Schwangerschaftsstreifen",
+        ingridiens = [
+            "50g Kakaobutter",
+            "50g Kokosöl",
+            "15Tropfen ätherisches Orangenöl"
+        ],
+        forWhat = "Stärkt das Bindegewebe der Haut. Am besten zweimal täglich einmassieren.",
+        zubereitung = "Kakaobutter und Kokosöl  im Wasserbad schmelzen. Ständig rühren bis die Salbe ca. 40 Grad Celsius hat. Danach das ätherische Orangenöl einrühren und in dunkle Glastiegel abfüllen."
+    ],
+    [
+        comment = "Seife",
+        nameOfIt = "Kamillenblüten Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Kamillenwasser oder Kamillentee",
+            "50ml Kamillenblütenölauszug"
+        ],
+        forWhat = "Besonders für zarte Haut (Kinder- Haut) geeignet.",
+        zubereitung = "Aus den Kamillenblüten einen Teeaufguss zubereiten. Ca. 2 Teelöffel Kamille mit 200ml kochenden Wasser übergießen und 10min. ziehen lassen. Oder das Kamillenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Kamillenwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Kamillen Ölauszug zugießen, gut verrühren, von der Platte nehmen und ein wenig abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Schafgarben Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Schafgarbenwasser oder Schafgarbentee",
+            "50ml Schafgarben Ölauszug"
+        ],
+        forWhat = "Die Schafgarbenseife ist angenehm für jeden Hauttyp.",
+        zubereitung = "Aus der Schafgarbe einen Teeaufguss zubereiten. Ca. 2 Teelöffel Schafgarbe mit 200ml kochenden Wasser übergießen und 10min. ziehen lassen. Oder das Schafgarbenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Kamillenwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Schafgarben Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Ringelblumen Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Ringelblumenwasser oder Ringelblumentee",
+            "50ml Ringelblumen Ölauszug"
+        ],
+        forWhat = "Die Ringelblumenseife wirkt durchblutungsfördernd.",
+        zubereitung = "Aus den Ringelblumen einen Teeaufguss zubereiten. Ca. 4 Teelöffel Ringelblumen mit 200ml kochenden Wasser übergießen und 15min. ziehen lassen. Oder das Ringelblumenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Ringelblumenwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Ringelblumen Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Melissen Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Melissenwasser oder Melissentee",
+            "50ml Melissen Ölauszug"
+        ],
+        forWhat = "Melissenseife ist eine sehr frische Seife mit zartem Zitrusduft.",
+        zubereitung = "Aus der Melisse einen Teeaufguss zubereiten. Ca. 3 Teelöffel Melisse mit 200ml kochenden Wasser übergießen und 10min. ziehen lassen. Oder das Melissenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Melissenwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Melissen Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Lindenblüten Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Lindenblütenwasser oder Lindenblütentee",
+            "50ml Lindenblüten Ölauszug"
+        ],
+        forWhat = "Eine sehr milde und pflegende Seife für jede Haut geeignet.",
+        zubereitung = "Aus den Lindenblüten einen Teeaufguss zubereiten. Ca. 3 Teelöffel Lindenblüten mit 200ml kochenden Wasser übergießen und 10min. ziehen lassen. Oder das Lindenblütenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Lindenblütenwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Lindenblüten Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Eibisch Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Eibischwasser oder Eibischtee",
+            "50ml Eibisch Ölauszug"
+        ],
+        forWhat = "Eine sehr milde und pflegende Seife für jede Haut geeignet.",
+        zubereitung = "Aus den Eibischwurzeln einen Teeaufguss zubereiten. Ca. 2 Teelöffel Eibisch mit 200ml kochenden Wasser übergießen und 15min. ziehen lassen. Oder das Eibischwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Eibischwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Eibisch Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Rosenblüten Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Rosenblütenwasser oder Rosenblütentee",
+            "50ml Rosenblüten Ölauszug"
+        ],
+        forWhat = "Wirkt beruhigend, glättend und straffend. Eine sehr pflegende Seife bei trockener und empfindlicher Haut.",
+        zubereitung = "Aus den Rosenblüten einen Teeaufguss zubereiten. Ca. 3 Teelöffel Rosenblüten mit 200ml kochenden Wasser übergießen und 15min. ziehen lassen. Oder das Rosenwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee (Rosenwasser) zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Rosenblüten Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ],
+    [
+        nameOfIt = "Lavendelblüten Seife",
+        ingridiens = [
+            "3g Bienenwachs",
+            "150g Kernseife",
+            "150ml Lavendelwasser oder Lavendeltee",
+            "50ml Lavendel Ölauszug"
+        ],
+        forWhat = "Wirkt sehr beruhigend, antibakteriell und hautregenerierend. Für jede Haut geeignet.",
+        zubereitung = "Aus den Lavendelblüten einen Teeaufguss zubereiten. Ca. 2 Teelöffel Lavendel mit 200ml kochenden Wasser übergießen und 10min. ziehen lassen. Oder das Lavendelwasser auf ca. 60 Grad Celsius erwärmen. Dann das Bienenwachs im Wasserbad schmelzen und den heißen Tee oder das Lavendelwasser zugießen. Umrühren und weiter erwärmen bis es leicht siedet. Nach und nach die geriebenen Seifenflocken (Kernseife mit einer Küchenraspel zu Flocken reiben) unterrühren, bis eine dicke Brühe entstanden ist, und diese kurz aufkochen lassen. Danach den Lavendel Ölauszug zugießen, gut verrühren, von der Platte nehmen und abkühlen lassen."
+    ]
+]
