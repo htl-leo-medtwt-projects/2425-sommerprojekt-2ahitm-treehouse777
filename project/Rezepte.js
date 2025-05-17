a31f2526846d33fd58a43cfca05bec0ec7786ced
Valerie Krolokh <v.krolokh@students.htl-leonding.ac.at>
2025-05-17 20:45:54 +0200

    Orte

--- project/Rezepte.js
+++ project/Rezepte.js
@@ -4,17 +4,17 @@ function main() {
     
     for (let i = 0; i < recepise.length; i++) {
         if (recepise[i].comment == "Salbe") {
-            output += '<div class="ordnung">Salbe</div><div>';
-            theComment = "Salbe";
+            output += '<div class="ordnungRezepte salbe">Salben</div><div class="containerRezepte">';
+            theComment = "Salben";
         } else if (recepise[i].comment == "Seife") {
-            output += '</div><div class="ordnung">Seife</div><div>';
-            theComment = "Seife";
+            output += '</div><div class="ordnungRezepte">Seifen</div><div class="containerRezepte containerSeife">';
+            theComment = "Seifen";
         }
 
         let ingridiens = "";
         for(let j = 0; j < recepise[i].ingridiens.length; j++) {
             if(recepise[i].ingridiens.length -1 >= j) {
-                ingridiens += recepise[i].ingridiens[j] + ",";
+                ingridiens += recepise[i].ingridiens[j] + ", ";
             }
             else {
                 ingridiens += recepise[i].ingridiens[j];
@@ -23,11 +23,11 @@ function main() {
         }
 
         output += `
-        <div class="smallContainer container${theComment}">
-            <p class="name">${recepise[i].nameOfIt}</p>
-            <p class="name">${ingridiens}</p>
-            <p class="name">${recepise[i].forWhat}</p>
-            <p class="name">${recepise[i].zubereitung}</p>
+        <div class="smallContainerRezepte">
+            <h1 class="name">${recepise[i].nameOfIt}</h1>
+            <p class="ingridiens">${ingridiens}</p>
+            <p class="forWhat">${recepise[i].forWhat}</p>
+            <p class="zubereitung">${recepise[i].zubereitung}</p>
         </div>`; /*class="smallContainer container${theComment}" onclick='openHerbModal(${JSON.stringify(recepise[i]).replace(g, "\\'")})' */
     }
     document.getElementById('outputRecepies').innerHTML = output;
